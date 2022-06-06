642db2233a474cb5026150cbc6d98bc8186b6643
01thn <tikhon.novitski@protonmail.com>
2022-06-06 14:35:30 +0300

    Initial commit

---
+++ src/main/java/com/thn/restpetstore/RestPetstoreApplication.java
@@ -0,0 +1,13 @@
+package com.thn.restpetstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPetstoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestPetstoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thn/restpetstore/RestPetstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thn.restpetstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPetstoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
